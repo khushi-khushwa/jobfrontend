c93ddf8c58001ab38f81bf40b8316b34b2220feb
khushi-khushwa <khushikhushwah70@gmail.com>
2024-08-20 16:02:40 +0530

    change style property of h1 tag

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import bannerImage from "../assets/images/process.png"
 const HeroSection = () => {
   return (
-    <div className='bg-emerald-600/5 '>
-      <div className=" flex justify-center items-center h-[855px]" >
-        <div>
-              <h1 className='sm:font-bold sm:text-4xl max-[630px]:font-bold text-2xl'> Welcome to the job portal  </h1>
+  
+      <div className='h-screen flex items-center justify-center bg-emerald-600/5'>
+       
+              <h1 className='  text-center  sm:font-bold sm:text-4xl max-[630px]:font-bold text-2xl'> Welcome to the job portal  </h1>
              
-              </div>
+         
         </div>
-    </div>
+
   )
 }
 
